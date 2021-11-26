9ee1db13aa19e6026dd4307ff8f557126cd2bb9e
wuxiaochen <wuxc.eng@gmail.com>
2021-11-26 15:32:52 +0800

    fix: bug修复

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -20,7 +20,7 @@ mkdir -p ./bin
 # build demo app
 for file in pkg/core/*; do
   app="$(basename $file)";
-  CGO_ENABLED=0 go build -tags prod -o bin/"$app" "./cmd/$app"
+  CGO_ENABLED=0 go build -tags prod -o ./bin/wiblog "./cmd/$app"
   # docker image
   docker buildx build --platform "$_platform" \
     -f "build/package/$app.Dockerfile" \
@@ -30,4 +30,4 @@ for file in pkg/core/*; do
 done
 
 # clean dir bin
-rm -rf ./bin
+#rm -rf ./bin
