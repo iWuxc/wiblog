b72544ecf2c9b1ec41ade8c60e03210ef0851a83
wuxiaochen <wuxc.eng@gmail.com>
2021-11-26 11:50:01 +0800

    fix: 自动化脚本部署

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env sh
 
-set -e
-
 _registry="$1"
 _tag="$2"
-#_platform="linux/amd64,linux/arm64,linux/386"
+_platform="linux/amd64,linux/arm64,linux/386"
 
 if [ -z "$_registry" ] || [ -z "$_tag" ]; then
   echo "Please specify image repository and tag."
-  exit 0
+  exit 0;
 fi
 
-# create build dir
-mkdir -p ../bin
+# create and use builder
+docker buildx inspect builder >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+  docker buildx create --use --name builder
+fi
 
-# create builder
-# docker buildx create --use --name builder
-
-for file in ../pkg/core/*; do
-  app="$(basename $file)"
-  CGO_ENABLED=0 go build -tags prod -o ../bin/wiblog "../cmd/$app"
-  #docker image
-  # docker build --platform "$_platform" \
-  docker build \
-  -f "../build/package/$app.Dockerfile" \
-  -t "$_registry/$app:latest" \
-  -t "$_registry/$app:$_tag" .
-#  --push .
+# prepare dir ./bin
+mkdir -p ./bin
+# build demo app
+for file in pkg/core/*; do
+  app="$(basename $file)";
+  CGO_ENABLED=0 go build -tags prod -o bin/backend "./cmd/$app"
+  # docker image
+  docker buildx build --platform "$_platform" \
+    -f "build/package/$app.Dockerfile" \
+    -t "$_registry/$app:latest" \
+    -t "$_registry/$app:$_tag" \
+    --push .
 done
 
 # clean dir ./bin
-rm -rf ../bin
+rm -rf ./binx
